0a7e8fc0fc4c73a8f98feb40e6274b84531bf5d5
gokul <iamgokul2401@gmail.com>
2022-06-09 15:31:52 +0530

    edit fourth

--- expressServerr/server.js
+++ expressServerr/server.js
@@ -1,22 +1,16 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-
+let bodyParser = require('body-parser')
 const { engine } = require('express-handlebars');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine','hbs')
 app.set('views','./template')
 
 app.use(express.static(__dirname + '/template'));
 app.engine('handlebars', engine({ extname: '.hbs', defaultLayout: "main"}));
-
-app.get('/update',(req,res)=>{
-    fs.readFile('./data.json','utf-8',(err,data)=>{
-        let x = JSON.parse(data)
-        
-    })
-})
 
 app.get('/',(req,res)=>{
     res.render('home')
@@ -27,10 +21,28 @@ app.get('/admin',(req,res)=>{
             let x = JSON.parse(data)
             res.render('admin',{ "data":x.items })
         })
-    
+        
     })
 })
 
+app.post('/save',(req,res)=>{
+    let name = req.body.name
+    let phno = req.body.phno
+    fs.readFile('./data.json','utf-8',(err,data)=>{
+        var x = JSON.parse(data)
+        x.items.push({"date":name,"phone":phno})
+        console.log(x);
+        fs.writeFile('./data.json',JSON.stringify(x),(err,d)=>{
+            console.log(err);
+        })
+    })
+    //  fs.writeFile('./data.json',x,()=>{
+    //      console.log("write");
+    //  })
+    
+    
+
+})
 
 app.listen(3000,'127.0.0.1',()=>{
     console.log("server start");
--- expressServerr/template/main.js
+++ expressServerr/template/main.js
@@ -2,10 +2,17 @@ document.getElementById('form').addEventListener('submit',(e)=>{
     let user = document.getElementById('name')
     let phno = document.getElementById('phno')
 
-    if(user.value.length<3 || user.value.match(/^[a-zA-Z]+$/) ){
-        alert("User Name Not valid")
+    // if(user.value.length < 3 || !user.value.match(/^[a-zA-Z]+$/) ){
+    //     alert("User Name Not valid")
+    // }
+    if(phno.value.length !=10 || !phno.value.match(/^[0-9]+$/) ){
+        alert("Enter valid phone number")
     }
     
-    e.preventDefault();
+    $.ajax({
+        url:'/save',
+        method:"post",
+        data:{name:user.value,phno:phno.value},
+    })
 
 })
